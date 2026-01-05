0566ee4f30487e36d4e565f007297f9e0571ac61
ValehK08 <valeh.kazimli@outlook.com>
2026-01-05 11:56:33 +0000

    .

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" FROM NUMPY """
+
+import pandas as pd
+def from_numpy(array):
+    """FROM NUMPY ARRAY TO DATAFRAME"""
+
+    cols = [i for i in "ABCDEFGHJKLMNOPQRSTUVWXYZ"]
+    df = pd.DataFrame(data=array, columns=cols[:array.shape[1]])
+    return df
