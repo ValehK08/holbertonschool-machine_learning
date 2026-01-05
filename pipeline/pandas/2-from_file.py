87fe007bbde466679049daacd4669e74f7c37345
ValehK08 <valeh.kazimli@outlook.com>
2026-01-05 12:11:08 +0000

    .

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -4,5 +4,11 @@ import pandas as pd
 
 
 def from_file(filename, delimiter):
+    """
+    function input -> filename, delimiter
+    function output -> dataframe
+    """
+
+    
     df = pd.read_csv(filename, delimiter=delimiter)
     return df
