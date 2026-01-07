32ccd6dddc508344da7ae21e8729b9ea404c6190
ValehK08 <valeh.kazimli@outlook.com>
2026-01-07 08:57:55 +0000

    .

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" 12-hierarchy.py """
+
+
+def hierarchy(df1, df2):
+    """
+    input -> df1, df2
+    output -> df with hierarchial index
+    """
+
+    index = __import__('10-index').index
+    df1 = index(df1)
+    df2 = index(df2)
+    df1 = df1.loc[1417411980:1417417981]
+    df2 = df2.loc[1417411980:1417417981]
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    df.reorder_levels([1, 0], axis=0)
+    df = df.sort_index()
+    return df
