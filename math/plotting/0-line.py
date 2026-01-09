f691e9861bd47c25347f026e25f8351d7ef93fa4
ValehK08 <valeh.kazimli@outlook.com>
2026-01-09 06:11:37 +0000

    .

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+""" 0-line.py """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
+    """ line graph """
+    
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
