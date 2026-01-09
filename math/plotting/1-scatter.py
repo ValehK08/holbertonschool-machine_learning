d7d99fb2dc12f731f778cd1e3b2b7eaea42d7e7f
ValehK08 <valeh.kazimli@outlook.com>
2026-01-09 06:24:41 +0000

    .

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -14,7 +14,7 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.scatter(x,y, color='m')
+    plt.scatter(x, y, color='m')
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
     plt.title("Men's Height vs Weight")
