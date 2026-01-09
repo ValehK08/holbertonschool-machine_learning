6d63d2a9c865d80fb542cbc5f5a390ffa4f1669e
ValehK08 <valeh.kazimli@outlook.com>
2026-01-09 06:49:11 +0000

    .

---
+++ math/plotting/3-two.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" 3-two.py """
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """ two """
+
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.axis([0, 20000, 0, 1])
+    plt.plot(x, y1, '--r')
+    plt.plot(x, y2, 'g')
+    plt.legend(['C-14', 'Ra-226'])
+    plt.show()
