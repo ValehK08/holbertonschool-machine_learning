0e9122c31cff67dffb3b75dc34596aa6543c77a3
ValehK08 <valeh.kazimli@outlook.com>
2026-01-13 11:12:46 +0000

    .

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 17-integrate.py """
+
+
+def poly_integral(poly, C=0):
+    """ polynomial integration """
+
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    elif not isinstance(C, (int, float)):
+        return None
+    else:
+        poly = [poly[x]/(x+1) for x in range(len(poly))
+        for k in range(len(poly)):
+            if int(poly[k]) == poly[k]:
+                poly[k] = int(k)
+        poly.insert(0, C)
+        return poly
