52bf23b9f18cb5aec90353ff7b2c1c23f8dfa527
ValehK08 <valeh.kazimli@outlook.com>
2026-01-23 12:54:21 +0000

    .

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" ACROSS THE PLANES """
+
+
+def add_matrices2D(mat1, mat2):
+    """ add matrices """
+    a = []
+    for i in range(len(mat1)):
+        a.append([
+            sum(k)
+            for k in list(zip(*[(mat1+mat2)[i]]+[(mat1+mat2)[i+len(mat1)]]))
+            ])
+    return a
