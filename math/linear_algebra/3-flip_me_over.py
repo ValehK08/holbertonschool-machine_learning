20f28fc348770cf9efb540634caaf6974a3c55e2
ValehK08 <valeh.kazimli@outlook.com>
2026-01-23 03:59:28 +0000

    .

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 """ SIZE ME PLEASE """
-a = [[]]
 
 
 def matrix_transpose(matrix):
     """ TRANSPOSE IT """
 
-    global a
-    for i in range(len(matrix[0])):
-        a.append([matrix[k][i] for k in range(len(matrix))])
-    return a
+    return [[matrix[i][k] for i in range(len(matrix))]
+            for k in range(len(matrix[0]))]
