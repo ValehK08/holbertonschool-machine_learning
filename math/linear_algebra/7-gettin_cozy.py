940cac3f50353ccc6d333649d8b5c68712c7a324
ValehK08 <valeh.kazimli@outlook.com>
2026-01-23 13:09:44 +0000

    .

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,6 +5,8 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """ cat the matrices """
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return mat1+mat2
     else:
         if len(mat1) != len(mat2):
