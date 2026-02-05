0512f115a5ec4a9428edb7794184cd79dee1c2b4
ValehK08 <valeh.kazimli@outlook.com>
2026-02-05 08:10:42 +0000

    .

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -3,27 +3,26 @@
 
 
 def determinant(matrix):
-    """ Find determinant of matrix """
+    """ Find determinant """
+
+    if any(not isinstance(i, list) for i in matrix):
+        raise TypeError("matrix must be a list of lists")
 
     if matrix == [[]]:
         return 1
-    
-    if matrix == []:
-        return 0
 
-    if not isinstance(matrix[0], list):
-        raise TypeError('matrix must be a list of lists') 
-    
-    if len(matrix) != len(matrix[0]):
-        raise ValueError('matrix must be a square matrix')
-    
+    if any(len(i) != len(matrix) for i in matrix):
+        raise ValueError("matrix must be a square matrix")
+
     if len(matrix) == 1:
-        det = matrix[0][0]
-    
-    if len(matrix[0]) == 2:
-        det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    
-    if len(matrix[0]) == 3:
-        det = matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[2][0] * matrix[1][2]) + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0])
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for i in range(len(matrix)):
+        minor = [row[:i] + row[i + 1:] for i in matrix[1:]]
+        det += ((-1) ** i) * matrix[0][i] * determinant(minor)
 
     return det
