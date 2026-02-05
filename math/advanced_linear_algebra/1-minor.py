893b70d0055c9fcff2a499a48c1a41cbc9057314
ValehK08 <valeh.kazimli@outlook.com>
2026-02-05 08:32:42 +0000

    .

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
 """ MINOR """
 
+
 def minor(matrix):
     """ FIND THE MINOR """
 
     if any(not isinstance(i, list) for i in matrix) or matrix == []:
         raise TypeError("matrix must be a list of lists")
-    
+
     if len(matrix) == 1 and len(matrix[0]) == 1:
         return [[1]]
 
     if any(len(i) != len(matrix) for i in matrix) or matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
-    
+
     m = [[] for _ in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            m[i].append(determinant([row[:j] + row[j+1:] for row in (matrix[:i]+matrix[i+1:])]))
+            m[i].append(determinant([
+                row[:j] + row[j+1:]
+                for row in (matrix[:i]+matrix[i+1:])
+                ]))
     return m
     
 
