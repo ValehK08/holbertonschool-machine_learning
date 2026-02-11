48f0bff9ec365ec2167f3379042ff3a6d4606f3e
ValehK08 <valeh.kazimli@outlook.com>
2026-02-11 09:20:56 +0000

    .

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -36,6 +36,8 @@ class Normal:
         return (z * self.stddev) + self.mean
 
     def pdf(self, x):
+        """ Probability Distribution Function """
+
         pi = 3.1415926536
         e = 2.7182818285
         f1 = (1 / (self.stddev * (2 * pi) ** 0.5))
