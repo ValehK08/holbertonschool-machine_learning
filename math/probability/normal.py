944c0528bb6155c4adb514d834bc35ec24de4bbe
ValehK08 <valeh.kazimli@outlook.com>
2026-02-11 09:20:21 +0000

    .

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -34,3 +34,10 @@ class Normal:
         """ x-value """
 
         return (z * self.stddev) + self.mean
+
+    def pdf(self, x):
+        pi = 3.1415926536
+        e = 2.7182818285
+        f1 = (1 / (self.stddev * (2 * pi) ** 0.5))
+        f2 = (e ** ((-1 / 2) * ((x - self.mean) / (self.stddev)) ** 2))
+        return f1 * f2
