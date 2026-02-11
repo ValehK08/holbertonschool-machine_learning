eb03b47e711a2b547f4f4b33d7f9b1c576e972ea
ValehK08 <valeh.kazimli@outlook.com>
2026-02-11 09:44:16 +0000

    .

---
+++ math/probability/binomial.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Binomial """
+
+
+class Binomial:
+    """ Binomial Class """
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """ INIT """
+
+        if data is None:
+            if n <= 0:
+                raise ValueError('n must be a positive value')
+            if p < 0 and p > 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.n = float(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            mean = float(sum(data) / len(data))
+            stdev = 0
+            for i in data:
+                stdev += (mean - i) ** 2
+            stdev = (stdev / len(data)) ** 0.5
+            self.p = float(1 - ((stdev ** 2) / mean))
+            self.n = float(mean / self.p)
