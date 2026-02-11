1b7f019077364d5970761962f91f0ba943786e08
ValehK08 <valeh.kazimli@outlook.com>
2026-02-11 09:11:54 +0000

    .

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -18,7 +18,7 @@ class Normal:
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
-            
+
             self.mean = float(sum(data) / len(data))
             s = 0
             for i in data:
