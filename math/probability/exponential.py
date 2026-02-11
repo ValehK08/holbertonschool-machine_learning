24038ae125e9832201d978ddf061c8056eec3419
ValehK08 <valeh.kazimli@outlook.com>
2026-02-11 09:00:46 +0000

    .

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -31,7 +31,7 @@ class Exponential:
     def cdf(self, x):
     """ Cumulative Distribution Function """
 
-    if x < 0:
-        return 0
+        if x < 0:
+            return 0
 
-    return 1 - (e ** (-1 * self.lambtha * x))
+        return 1 - (e ** (-1 * self.lambtha * x))
