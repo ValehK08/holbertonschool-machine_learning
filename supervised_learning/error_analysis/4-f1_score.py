0c93477d0a846358fb101a4a822eec0b3d848cd3
ValehK08 <valeh.kazimli@outlook.com>
2026-02-16 08:50:06 +0000

    .

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -4,6 +4,7 @@ import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
+
 def f1_score(confusion):
     """ F1 Score """
     sens = sensitivity(confusion)
