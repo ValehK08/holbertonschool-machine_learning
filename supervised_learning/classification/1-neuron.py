1d5c2715045a71760cd753d3ea4107a98455b3e0
ValehK08 <valeh.kazimli@outlook.com>
2026-03-06 08:40:44 +0000

    .

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" 1-neuron.py """
+import numpy as np
+
+
+class Neuron:
+    """ Neuron Class """
+
+    def __init__(self, nx):
+        """ Instantiation """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ W getter """
+        return self.__W
+
+    @property
+    def b(self):
+        """ b getter """
+        return self.__b
+
+    @property
+    def A(self):
+        """ A getter """
+        return self.__A
