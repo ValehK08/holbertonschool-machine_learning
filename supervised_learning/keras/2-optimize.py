212fb8993fa580a938d0279b210e407f6fa0abc1
ValehK08 <valeh.kazimli@outlook.com>
2026-03-12 03:38:36 +0000

    .

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 2-optimize.py """
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """ optimizer """
+
+    network.compile(K.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2
+    ))
+    return None
