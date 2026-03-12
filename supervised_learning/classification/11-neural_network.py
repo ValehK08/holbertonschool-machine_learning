e425638f019eea4dd3d9c7687899950bbcf8c0d7
ValehK08 <valeh.kazimli@outlook.com>
2026-03-12 04:17:55 +0000

    .

---
+++ supervised_learning/classification/11-neural_network.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+""" 11-neural_network.py """
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Neural Network class """
+
+    def __init__(self, nx, nodes):
+        """ initialize """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """ getter """
+        return (self.__W1)
+
+    @property
+    def b1(self):
+        """ getter """
+        return (self.__b1)
+
+    @property
+    def A1(self):
+        """ getter """
+        return (self.__A1)
+
+    @property
+    def W2(self):
+        """ getter """
+        return (self.__W2)
+
+    @property
+    def b2(self):
+        """ getter """
+        return (self.__b2)
+
+    @property
+    def A2(self):
+        """ getter """
+        return (self.__A2)
+
+    def forward_prop(self, X):
+        """ forward prop """
+        self.__A1 = 1 / (1 + (np.exp(-1 * np.matmul(self.W1, X) + self.b1)))
+        self.__A2 = 1 / (1 + (np.exp(-1 * np.matmul(self.W2, self.__A1) + self.b2)))
+        return (self.A1, self.A2)
+
+    def cost(self, Y, A):
+        """ cost """
+        m = Y.shape[1]
+        cost = -(np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))))
+        cost *= (1 / m)
+        return cost
