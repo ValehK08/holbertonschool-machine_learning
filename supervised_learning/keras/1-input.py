ff0ac283f9cd4c46e1333fe2946635d0a3c5c82d
ValehK08 <valeh.kazimli@outlook.com>
2026-03-12 03:32:10 +0000

    .

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" 1-input.py """
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """ build model """
+
+    inputs = K.Input(shape=(nx,))
+    x = K.layers.Dense(units=layers[0], activation=activations[0], kernel_regularizer=K.regularizers.l2(lambtha))(inputs)
+    for i in range(1, len(layers)):
+        x = K.layers.Dropout(1 - keep_prob)(x)
+        x = K.layers.Dense(units=layers[i], activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha))(x)
+    model = K.Model(inputs=inputs, outputs=x)
+    return model
