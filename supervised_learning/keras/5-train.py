763102e2756e058be7709965717b229187b9d98d
ValehK08 <valeh.kazimli@outlook.com>
2026-03-12 04:09:18 +0000

    .

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" 5-train.py """
+import tensorflow.keras as K
+
+
+def train_model(
+    network,
+    data,
+    labels,
+    batch_size,
+    epochs,
+    validation_data=None,
+    verbose=True, shuffle=False):
+    """ train """
+
+    history = network.fit(
+        x=data, y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+    return history
