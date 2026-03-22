ded4a65ef094bf1820790fa37db062390c467e25
Valeh Kazimli <valeh.kazimli@outlook.com>
2026-03-22 20:28:49 +0400

    Refactor train_model function parameters and docstring
    Refactor train_model function for improved readability.

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-""" 4-train.py """
+"""4-train.py"""
+
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
-    """ train model """
-
-    return network.fit(
-        x=data,
-        y=labels,
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
+    """Train model"""
+    history = network.fit(
+        data,
+        labels,
         batch_size=batch_size,
         epochs=epochs,
         verbose=verbose,
         shuffle=shuffle
     )
+    return history
