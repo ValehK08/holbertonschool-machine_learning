13514bdf167717b94a2058f498e1cbbca2f9884d
ValehK08 <valehkazimli@outlook.com>
2026-03-30 16:42:28 +0000

    .

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -5,12 +5,12 @@ import numpy as np
 
 def convolve_grayscale_padding(images, kernel, padding):
     """Convolution on Greyscale, w/ padding"""
-    m, h, w = images.shape[0], images.shape[1], images.shape[2]
     kh, kw = kernel.shape[0], kernel.shape[1]
     images = np.pad(images, (
         (0, 0), (padding[0], padding[0]), (padding[1], padding[1])
     ), constant_values=0)
-    conv = np.zeros((m, images.shape[1] - kh + 1, images.shape[2] - kw + 1))
+    m, h, w = images.shape[0], images.shape[1], images.shape[2]
+    conv = np.zeros((m, h - kh + 1, w - kw + 1))
     for i in range(h - kh + 1):
         for j in range(w - kw + 1):
             part = images[:, i:i + kh, j:j + kw] * kernel
