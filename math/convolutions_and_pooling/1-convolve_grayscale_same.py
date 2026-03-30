4906f4e6ca955a37b8ea750b57fd0abc02e56b06
ValehK08 <valehkazimli@outlook.com>
2026-03-30 16:37:04 +0000

    .

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """Convolution on greyscale, w/ padding"""
-    
+
     m, h, w = images.shape
     kh, kw = kernel.shape
     ph = kh // 2
@@ -16,5 +16,5 @@ def convolve_grayscale_same(images, kernel):
         for j in range(w):
             part = padded[:, i:i + kh, j:j + kw] * kernel
             conv[:, i, j] = np.sum(part, axis=(1, 2))
-    
+
     return conv
