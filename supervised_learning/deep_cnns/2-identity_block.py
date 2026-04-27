6921e3b1422235285f39e9e672413033013da1a1
ValehK08 <valeh.kazimli@outlook.com>
2026-04-27 14:41:33 +0000

    .

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 """ 2-identity_block.py """
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
     """I-block"""
 
     F11, F3, F12 = filters
-    he_init = initializers.he_normal(seed=0)
+    he_init = K.initializers.he_normal(seed=0)
 
     X_shortcut = A_prev
 
-    X = layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(1, 1),
+    X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(1, 1),
                       padding='valid', kernel_initializer=he_init)(A_prev)
-    X = layers.BatchNormalization(axis=3)(X)
-    X = layers.Activation('relu')(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
 
-    X = layers.Conv2D(filters=F3, kernel_size=(3, 3), strides=(1, 1),
+    X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), strides=(1, 1),
                       padding='same', kernel_initializer=he_init)(X)
-    X = layers.BatchNormalization(axis=3)(X)
-    X = layers.Activation('relu')(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
 
-    X = layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1),
+    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1),
                       padding='valid', kernel_initializer=he_init)(X)
-    X = layers.BatchNormalization(axis=3)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
 
-    X = layers.Add()([X, X_shortcut])
-    X = layers.Activation('relu')(X)
+    X = K.layers.Add()([X, X_shortcut])
+    X = K.layers.Activation('relu')(X)
 
     return X
